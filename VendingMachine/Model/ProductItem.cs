466cf7d036292cfac8c91a432eab019942736896
rababalsurmi <rababalsurmi@gmail.com>
2021-10-19 11:35:18 +0200

    Created Abstract Class (ProductItem)

---
+++ VendingMachine/Model/ProductItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace VendingMachine.Model
+{
+    public abstract class ProductItem
+    {
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int RemainingItems { get; set; }
+
+        public string VendedMessage { get; set; }
+
+        public string SoldOutMessage { get; set; }
+
+        public ProductItem()
+        {
+
+        }
+
+        public ProductItem(string productName, decimal price, int remainingitems, string vendedMessage)
+        {
+            ProductName = productName;
+            Price = price;
+            RemainingItems = remainingitems;
+            VendedMessage = vendedMessage;
+            SoldOutMessage = $"Sold out of {this.ProductName}!\nBuy something else!";
+        }
+
+        public bool RemoveItem()
+        {
+            if (RemainingItems >0)
+            {
+                RemainingItems--;
+                return true;
+            }
+            return false;
+        }
+    }
+}
